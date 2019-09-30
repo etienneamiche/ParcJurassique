7be0dc99683af0f5048c301f1e88740f276a11b8
etienneamiche <etienne.amiche@gmail.com>
2019-09-30 13:56:39 +0200

    fix du calcul de la valeur prix courant

--- client-parc-jurassique/src/data/game_data.js
+++ client-parc-jurassique/src/data/game_data.js
@@ -3,40 +3,35 @@ const gameData = {
     {
       'name': 'Styracosaure',
       'decription': '',
-      'prix_original': 10,
-      'prix_courant': 10,
+      'prix': 10,
       'danger': 1,
       'visiteur': 1,
       'logo': 'styracosaurus.png'
     }, {
       'name': 'Brachiosaure',
       'decription': '',
-      'prix_original': 100,
-      'prix_courant': 100,
+      'prix': 100,
       'danger': 2,
       'visiteur': 10,
       'logo': 'brachiosaure.png'
     }, {
       'name': 'Pterosaure',
       'decription': '',
-      'prix_original': 1000,
-      'prix_courant': 1000,
+      'prix': 1000,
       'danger': 3,
       'visiteur': 20,
       'logo': 'pterosaure.png'
     }, {
       'name': 'Velociraptor',
       'decription': '',
-      'prix_original': 10000,
-      'prix_courant': 10000,
+      'prix': 10000,
       'danger': 4,
       'visiteur': 50,
       'logo': 'velociraptor.png'
     }, {
       'name': 'Tyranosaure',
       'decription': '',
-      'prix_original': 100000,
-      'prix_courant': 100000,
+      'prix': 100000,
       'danger': 5,
       'visiteur': 100,
       'logo': 'tyranosaure.png'
@@ -45,58 +40,50 @@ const gameData = {
   'magasins': [{
     'name': 'Boutique de souvenirs',
     'decription': '',
-    'prix_original': 10,
-    'prix_courant': 10,
+    'prix': 10,
     'benefice': 1,
     'logo': 'giftshop.png'
   }, {
     'name': 'Restaurant',
     'decription': '',
-    'prix_original': 100,
-    'prix_courant': 100,
+    'prix': 100,
     'benefice': 2,
     'logo': 'restaurant.png'
   }, {
     'name': 'Photographe',
     'decription': '',
-    'prix_original': 200,
-    'prix_courant': 200,
+    'prix': 200,
     'benefice': 3,
     'logo': 'photographer.png'
   }, {
     'name': 'Nurserie',
     'decription': '',
-    'prix_original': 500,
-    'prix_courant': 500,
+    'prix': 500,
     'benefice': 4,
     'logo': 'eggs.png'
   }],
   'personnels': [{
     'name': 'Gardien',
     'decription': 'petit gardien pas très efficace',
-    'prix_original': 10,
-    'prix_courant': 10,
+    'prix': 10,
     'danger': -1,
     'logo': 'policeman.png'
   }, {
     'name': 'Veterinaire',
     'decription': '',
-    'prix_original': 100,
-    'prix_courant': 100,
+    'prix': 100,
     'danger': -2,
     'logo': 'veterinarian.png'
   }, {
     'name': 'Soldat',
     'decription': '',
-    'prix_original': 1000,
-    'prix_courant': 1000,
+    'prix': 1000,
     'danger': -3,
     'logo': 'army.png'
   }, {
     'name': 'Hélicoptère',
     'decription': '',
-    'prix_original': 10000,
-    'prix_courant': 10000,
+    'prix': 10000,
     'danger': -4,
     'logo': 'helicopter.png'
   }]
